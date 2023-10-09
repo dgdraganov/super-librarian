c19a8cd0bf36cbd363086b655283f594e7198ceb
dimitar.draganov <dimitar.g.draganov@gmail.com>
2023-10-09 19:16:25 +0300

    Added migrations

---
+++ internal/migrations/1_create_books_table.up.sql
@@ -0,0 +1,11 @@
+
+
+CREATE TABLE Books (
+    ID int NOT NULL,
+    Author varchar(100) NOT NULL,
+    Title varchar(255) NOT NULL,
+    BookCover varchar(1024) NOT NULL,
+    PublishedAt DATE NOT NULL,
+    PRIMARY KEY (ID)
+);
+
